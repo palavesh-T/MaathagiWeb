b239d60ce30da6738885efcee379aab9a53741cd
palavesh-T <palavesamuthumathi@gmail.com>
2025-06-03 18:05:44 +0530

    test

--- src/Admin/Changepassword.js
+++ src/Admin/Changepassword.js
@@ -2,24 +2,24 @@ import React, { useState } from "react";
 import { useNavigate } from 'react-router-dom';
 import "../Admin/assete/css/changepassword.css";
 import Adminheader from "./Adminheader";
-import {updateConfirmPassword} from '../service/Api';
+import { updateConfirmPassword } from '../service/Api';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 import Adminfooter from "./Adminfooter";
 
 
-const Changepassword = () =>{
+const Changepassword = () => {
 
-  const navigate = useNavigate();
-  const [currentPassword, setCurrentPassword] = useState('');
-  const [newPassword, setNewPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
-   const [errorMessage, setErrorMessage] = useState('');
+    const navigate = useNavigate();
+    const [currentPassword, setCurrentPassword] = useState('');
+    const [newPassword, setNewPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+    const [errorMessage, setErrorMessage] = useState('');
     const [currentPasswordVisible, setCurrentPasswordVisible] = useState(false);
     const [newPasswordVisible, setNewPasswordVisible] = useState(false);
     const [confirmPasswordVisible, setConfirmPasswordVisible] = useState(false);
 
-     const toggleCurrentPasswordVisibility = () => {
+    const toggleCurrentPasswordVisibility = () => {
         setCurrentPasswordVisible(!currentPasswordVisible);
     };
 
@@ -32,14 +32,14 @@ const Changepassword = () =>{
     };
 
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
+    const handleSubmit = async (event) => {
+        event.preventDefault();
 
-    if (newPassword !== confirmPassword) {
-       setErrorMessage('New password and confirm password do not match');
-      return;
-    }
- try {
+        if (newPassword !== confirmPassword) {
+            setErrorMessage('New password and confirm password do not match');
+            return;
+        }
+        try {
             const response = await updateConfirmPassword({
                 id: 1,
                 current_password: currentPassword,
@@ -60,21 +60,21 @@ const Changepassword = () =>{
         }
     };
 
-    return(
+    return (
         <>
-        <Adminheader/>
-         <div className="change-password-container">
-      <form className="change-password-form" onSubmit={handleSubmit}>
-         <div className="form-group" style={{ position: 'relative' }}>
-        <h2>Change Password</h2>
-        <input
-          type={currentPasswordVisible ? "text" : "password"}
-          placeholder="current Password"
-           value={currentPassword}
-          onChange={(e) => setCurrentPassword(e.target.value)}
-          required
-        />
-         <FontAwesomeIcon
+            <Adminheader />
+            <div className="change-password-container">
+                <form className="change-password-form" onSubmit={handleSubmit}>
+                    <div className="form-group" style={{ position: 'relative' }}>
+                        <h2>Change Password</h2>
+                        <input
+                            type={currentPasswordVisible ? "text" : "password"}
+                            placeholder="current Password"
+                            value={currentPassword}
+                            onChange={(e) => setCurrentPassword(e.target.value)}
+                            required
+                        />
+                        <FontAwesomeIcon
                             icon={currentPasswordVisible ? faEye : faEyeSlash}
                             onClick={toggleCurrentPasswordVisibility}
                             className="icon"
@@ -86,16 +86,16 @@ const Changepassword = () =>{
                                 cursor: 'pointer'
                             }}
                         />
-                        </div>
-                         <div className="form-group" style={{ position: 'relative' }}>
-        <input
-           type={newPasswordVisible ? "text" : "password"}
-          placeholder="New Password"
-          value={newPassword}
-          onChange={(e) => setNewPassword(e.target.value)}
-          required
-        />
-         <FontAwesomeIcon
+                    </div>
+                    <div className="form-group" style={{ position: 'relative' }}>
+                        <input
+                            type={newPasswordVisible ? "text" : "password"}
+                            placeholder="New Password"
+                            value={newPassword}
+                            onChange={(e) => setNewPassword(e.target.value)}
+                            required
+                        />
+                        <FontAwesomeIcon
                             icon={newPasswordVisible ? faEye : faEyeSlash}
                             onClick={toggleNewPasswordVisibility}
                             className="icon"
@@ -108,15 +108,15 @@ const Changepassword = () =>{
                             }}
                         />
                     </div>
-                     <div className="form-group" style={{ position: 'relative' }}>
-        <input
-           type={confirmPasswordVisible ? "text" : "password"}
-          placeholder="Confirm Password"
-          value={confirmPassword}
-          onChange={(e) => setConfirmPassword(e.target.value)}
-          required
-        />
-         <FontAwesomeIcon
+                    <div className="form-group" style={{ position: 'relative' }}>
+                        <input
+                            type={confirmPasswordVisible ? "text" : "password"}
+                            placeholder="Confirm Password"
+                            value={confirmPassword}
+                            onChange={(e) => setConfirmPassword(e.target.value)}
+                            required
+                        />
+                        <FontAwesomeIcon
                             icon={confirmPasswordVisible ? faEye : faEyeSlash}
                             onClick={toggleConfirmPasswordVisibility}
                             className="icon"
@@ -128,12 +128,12 @@ const Changepassword = () =>{
                                 cursor: 'pointer'
                             }}
                         />
-                        </div>
-                         {errorMessage && <p className="error-message">{errorMessage}</p>}
-        <button type="submit">Update Password</button>
-      </form>
-    </div>
-    <Adminfooter/>
+                    </div>
+                    {errorMessage && <p className="error-message">{errorMessage}</p>}
+                    <button type="submit">Update Password</button>
+                </form>
+            </div>
+            <Adminfooter />
         </>
     )
 }
